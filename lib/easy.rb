17dbbfb5534506653b01d8c98ec8a28c9bc1cbb0
tsai810417 <tsai_cheng@yahoo.com>
2018-05-14 18:29:36 -0700

    add question

--- lib/easy.rb
+++ lib/easy.rb
@@ -13,3 +13,20 @@ def two_sum(nums, target)
         end
     end
 end
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+#
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
