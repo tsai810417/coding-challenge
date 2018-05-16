739e7315c3f7d767959f357c943090c036464d3b
tsai810417 <tsai_cheng@yahoo.com>
2018-05-15 17:36:17 -0700

    download Rotate String

--- lib/easy.rb
+++ lib/easy.rb
@@ -172,3 +172,24 @@ def reverse_word(str)
 
     res
 end
+
+# Rotate String
+#
+# We are given two strings, A and B.
+#
+# A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+#
+# Example 1:
+# Input: A = 'abcde', B = 'cdeab'
+# Output: true
+#
+# Example 2:
+# Input: A = 'abcde', B = 'abced'
+# Output: false
+# Note:
+#
+# A and B will have length at most 100.
+
+# @param {String} a
+# @param {String} b
+# @return {Boolean}
