a3405cf947bae5536a4c6d15fc12058c6e05c3c2
tsai810417 <tsai_cheng@yahoo.com>
2018-05-15 17:38:34 -0700

    download Number of Segments in a String

--- lib/easy.rb
+++ lib/easy.rb
@@ -241,3 +241,17 @@ def swap(s, k)
         s[0...k].reverse
     end
 end
+
+# Number of Segments in a String
+# 
+# Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+#
+# Please note that the string does not contain any non-printable characters.
+#
+# Example:
+#
+# Input: "Hello, my name is John"
+# Output: 5
+
+# @param {String} s
+# @return {Integer}
