a8143895aee371e271f17618e3701d5311e064a9
tsai810417 <tsai_cheng@yahoo.com>
2018-05-18 17:10:22 -0700

    download Fizz Buzz

--- lib/easy.rb
+++ lib/easy.rb
@@ -484,3 +484,34 @@ def distribute_candies(candies)
         return hash.keys.length
     end
 end
+
+# 412. Fizz Buzz
+#
+# Write a program that outputs the string representation of numbers from 1 to n.
+#
+# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+#
+# Example:
+#
+# n = 15,
+#
+# Return:
+# [
+#     "1",
+#     "2",
+#     "Fizz",
+#     "4",
+#     "Buzz",
+#     "Fizz",
+#     "7",
+#     "8",
+#     "Fizz",
+#     "Buzz",
+#     "11",
+#     "Fizz",
+#     "13",
+#     "14",
+#     "FizzBuzz"
+# ]
+# @param {Integer} n
+# @return {String[]}
