bf9c70f0449be7a834b0ccab6cf682bd7559b6e4
tsai810417 <tsai_cheng@yahoo.com>
2018-05-22 16:28:48 -0700

    download Longest Common Prefix

--- lib/easy.rb
+++ lib/easy.rb
@@ -1233,3 +1233,23 @@ def merge(nums1, m, nums2, n)
        end
     end
 end
+
+# 14. Longest Common Prefix
+# Write a function to find the longest common prefix string amongst an array of strings.
+#
+# If there is no common prefix, return an empty string "".
+#
+# Example 1:
+#
+# Input: ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+#
+# Input: ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+# Note:
+#
+# All given inputs are in lowercase letters a-z.
+# @param {String[]} strs
+# @return {String}
