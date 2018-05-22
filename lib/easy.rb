58c72adbf9aea0125f97fd0a50b8358012097591
tsai810417 <tsai_cheng@yahoo.com>
2018-05-22 10:17:10 -0700

    download Valid Palindrome

--- lib/easy.rb
+++ lib/easy.rb
@@ -1122,3 +1122,19 @@ end
 def sort(str)
   str.chars.sort.join
 end
+
+# 125. Valid Palindrome
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+#
+# Note: For the purpose of this problem, we define empty string as valid palindrome.
+#
+# Example 1:
+#
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
+# Example 2:
+#
+# Input: "race a car"
+# Output: false
+# @param {String} s
+# @return {Boolean}
