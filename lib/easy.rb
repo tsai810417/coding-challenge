3ac774b6025eea2e8fdd645d7e3791990644c74c
tsai810417 <tsai_cheng@yahoo.com>
2018-05-21 20:56:44 -0700

    download Product of Array Except Self

--- lib/easy.rb
+++ lib/easy.rb
@@ -1046,3 +1046,18 @@ def rotate(nums, k)
     nums[k..-1] = nums[k..-1].reverse
     nums[0...k] = nums[0...k].reverse
 end
+
+# 238. Product of Array Except Self
+#
+# Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+#
+# Example:
+#
+# Input:  [1,2,3,4]
+# Output: [24,12,8,6]
+# Note: Please solve it without division and in O(n).
+#
+# Follow up:
+# Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+# @param {Integer[]} nums
+# @return {Integer[]}
