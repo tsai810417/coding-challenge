b56442e0506eaf595571757e66af5c651910ed90
tsai810417 <tsai_cheng@yahoo.com>
2018-05-23 23:43:23 -0700

    download Convert Sorted Array to Binary Search Tree

--- lib/easy.rb
+++ lib/easy.rb
@@ -1289,3 +1289,32 @@ def missing_number(nums)
         return i if sorted[i] != i
     end
 end
+
+# 108. Convert Sorted Array to Binary Search Tree
+# Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+#
+# For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+#
+# Example:
+#
+# Given the sorted array: [-10,-3,0,5,9],
+#
+# One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+#
+#       0
+#      / \
+#    -3   9
+#    /   /
+#  -10  5
+
+ # Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {Integer[]} nums
+# @return {TreeNode}
