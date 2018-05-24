53a190fd29c303812f1afad0e66092ac7fb234ba
tsai810417 <tsai_cheng@yahoo.com>
2018-05-24 07:29:15 -0700

    download Intersection of Two Arrays II

--- lib/easy.rb
+++ lib/easy.rb
@@ -1362,3 +1362,20 @@ def maximum_product(nums)
         return sorted[-3..-1].reduce(:*)
     end
 end
+
+# 350. Intersection of Two Arrays II
+# Given two arrays, write a function to compute their intersection.
+#
+# Example:
+# Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+#
+# Note:
+# Each element in the result should appear as many times as it shows in both arrays.
+# The result can be in any order.
+# Follow up:
+# What if the given array is already sorted? How would you optimize your algorithm?
+# What if nums1's size is small compared to nums2's size? Which algorithm is better?
+# What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[]}
