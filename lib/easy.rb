d26b0674ab32e921df2c757b1a919383ed4ea755
tsai810417 <tsai_cheng@yahoo.com>
2018-05-25 09:43:45 -0700

    download Permutations

--- lib/easy.rb
+++ lib/easy.rb
@@ -1583,3 +1583,21 @@ def is_same_tree(p, q)
 
 
 end
+
+# 46. Permutations
+# Given a collection of distinct integers, return all possible permutations.
+#
+# Example:
+#
+# Input: [1,2,3]
+# Output:
+# [
+#   [1,2,3],
+#   [1,3,2],
+#   [2,1,3],
+#   [2,3,1],
+#   [3,1,2],
+#   [3,2,1]
+# ]
+# @param {Integer[]} nums
+# @return {Integer[][]}
