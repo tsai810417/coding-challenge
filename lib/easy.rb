9569e10c31a152f8b7b7286c39be13928767266e
tsai810417 <tsai_cheng@yahoo.com>
2018-05-25 08:50:41 -0700

    download Binary Tree Inorder Traversal

--- lib/easy.rb
+++ lib/easy.rb
@@ -1432,3 +1432,29 @@ def convert(n)
     end
     return str
 end
+
+# 94. Binary Tree Inorder Traversal
+# Given a binary tree, return the inorder traversal of its nodes' values.
+#
+# Example:
+#
+# Input: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+#
+# Output: [1,3,2]
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[]}
