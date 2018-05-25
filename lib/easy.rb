850110b066fc96c114e5883e8b761f3417d8e4c0
tsai810417 <tsai_cheng@yahoo.com>
2018-05-25 09:35:51 -0700

    download Same Tree

--- lib/easy.rb
+++ lib/easy.rb
@@ -1529,3 +1529,48 @@ def roman_to_int(s)
 
     sum += hash[s[-1].to_sym]
 end
+
+# 100. Same Tree
+# Given two binary trees, write a function to check if they are the same or not.
+#
+# Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+#
+# Example 1:
+#
+# Input:     1         1
+#           / \       / \
+#          2   3     2   3
+#
+#         [1,2,3],   [1,2,3]
+#
+# Output: true
+# Example 2:
+#
+# Input:     1         1
+#           /           \
+#          2             2
+#
+#         [1,2],     [1,null,2]
+#
+# Output: false
+# Example 3:
+#
+# Input:     1         1
+#           / \       / \
+#          2   1     1   2
+#
+#         [1,2,1],   [1,1,2]
+#
+# Output: false
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
