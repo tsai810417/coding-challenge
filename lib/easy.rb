56b3cf93219194803311f44b45624a16336ed151
tsai810417 <tsai_cheng@yahoo.com>
2018-05-25 13:12:51 -0700

    download Move Zeroes

--- lib/easy.rb
+++ lib/easy.rb
@@ -1799,7 +1799,21 @@ end
 # @param {Integer} num
 # @return {Integer}
 def add_digits(num)
-  # this is recursive solution  
+  # this is recursive solution
   return num if num < 10
   return add_digits(num % 10 + num / 10)
 end
+
+# 283. Move Zeroes
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+#
+# Example:
+#
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Note:
+#
+# You must do this in-place without making a copy of the array.
+# Minimize the total number of operations.
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
