7ef5a2a3613411894479333911ba2866c764d8e8
tsai810417 <tsai_cheng@yahoo.com>
2018-05-25 13:12:58 -0700

    solve Move Zeroes

--- lib/easy.rb
+++ lib/easy.rb
@@ -1817,3 +1817,25 @@ end
 # Minimize the total number of operations.
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
+def move_zeroes(nums)
+  # this solution runtime is 1000ms => too slow
+  # check = false
+  # while check == false
+  #     check = true
+  #    nums.each_index do |i|
+  #        next if i == nums.length - 1
+  #       if nums[i] == 0 && nums[i+1] != 0
+  #           check = false
+  #           nums[i+1], nums[i] = nums[i], nums[i+1]
+  #           next
+  #       end
+  #    end
+  # end
+
+  # this solution runtime is 48ms => faster
+  count = nums.count{|x| x == 0}
+  nums.delete(0)
+  count.times do
+    nums.push(0)
+  end
+end
