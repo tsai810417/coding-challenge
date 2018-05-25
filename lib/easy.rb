3673573f60c2fc69d7d3d772d21ac7bbb5f1554d
tsai810417 <tsai_cheng@yahoo.com>
2018-05-25 11:28:44 -0700

    download Single Number

--- lib/easy.rb
+++ lib/easy.rb
@@ -1639,3 +1639,21 @@ end
 def is_anagram(s, t)
     s.chars.sort == t.chars.sort
 end
+
+# 136. Single Number
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+#
+# Note:
+#
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+#
+# Example 1:
+#
+# Input: [2,2,1]
+# Output: 1
+# Example 2:
+#
+# Input: [4,1,2,1,2]
+# Output: 4
+# @param {Integer[]} nums
+# @return {Integer}
