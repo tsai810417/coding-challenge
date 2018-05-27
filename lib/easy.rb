d835e928a43ce5b23b339bdaad7088604713e6f1
tsai810417 <tsai_cheng@yahoo.com>
2018-05-26 22:03:01 -0700

    download Pascal's Triangle

--- lib/easy.rb
+++ lib/easy.rb
@@ -1992,3 +1992,21 @@ end
 def plus_one(digits)
     (digits.join.to_i + 1).to_s.chars.map{|x| x.to_i}
 end
+
+# 118. Pascal's Triangle
+# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+# In Pascal's triangle, each number is the sum of the two numbers directly above it.
+#
+# Example:
+#
+# Input: 5
+# Output:
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+# @param {Integer} num_rows
+# @return {Integer[][]}
