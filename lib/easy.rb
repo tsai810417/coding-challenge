6dc68103801a63b95ed0023afed54233cc24fa40
tsai810417 <tsai_cheng@yahoo.com>
2018-05-26 22:06:58 -0700

    download Kth Largest Element in an Array

--- lib/easy.rb
+++ lib/easy.rb
@@ -2031,3 +2031,20 @@ def create_new(arr)
 
     new_arr.unshift(1).push(1)
 end
+
+# 215. Kth Largest Element in an Array
+# Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+#
+# Example 1:
+#
+# Input: [3,2,1,5,6,4] and k = 2
+# Output: 5
+# Example 2:
+#
+# Input: [3,2,3,1,2,4,5,5,6] and k = 4
+# Output: 4
+# Note:
+# You may assume k is always valid, 1 ≤ k ≤ array's length.
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
