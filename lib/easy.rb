362af0106ae72f580a3f507c375ecd6ceccdb93b
tsai810417 <tsai_cheng@yahoo.com>
2018-06-13 14:31:49 -0700

    download Top K Frequent Elements

--- lib/easy.rb
+++ lib/easy.rb
@@ -2179,3 +2179,16 @@ def get_sum(a, b)
     arr = [a,b]
     arr.sum
 end
+
+# 347. Top K Frequent Elements
+# Given a non-empty array of integers, return the k most frequent elements.
+#
+# For example,
+# Given [1,1,1,2,2,3] and k = 2, return [1,2].
+#
+# Note:
+# You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+# Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer[]}
