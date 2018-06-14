fe0b202ee236c2b44b640d9f33121388beaf04e4
tsai810417 <tsai_cheng@yahoo.com>
2018-06-13 18:45:12 -0700

    download Contains Duplicate

--- lib/easy.rb
+++ lib/easy.rb
@@ -2199,3 +2199,23 @@ def top_k_frequent(nums, k)
     end
     hash.keys.sort_by{|x| -hash[x]}[0...k]
 end
+
+# 217. Contains Duplicate
+# Given an array of integers, find if the array contains any duplicates.
+#
+# Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+#
+# Example 1:
+#
+# Input: [1,2,3,1]
+# Output: true
+# Example 2:
+#
+# Input: [1,2,3,4]
+# Output: false
+# Example 3:
+#
+# Input: [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+# @param {Integer[]} nums
+# @return {Boolean}
