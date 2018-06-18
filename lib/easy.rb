a7466f1fe292d229e68cf93f24a6736f5a38eb72
tsai810417 <tsai_cheng@yahoo.com>
2018-06-17 20:53:42 -0700

    download Merge Two Sorted Lists

--- lib/easy.rb
+++ lib/easy.rb
@@ -2222,3 +2222,23 @@ end
 def contains_duplicate(nums)
     nums.uniq.length != nums.length
 end
+
+# 21. Merge Two Sorted Lists
+# Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+#
+# Example:
+#
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
