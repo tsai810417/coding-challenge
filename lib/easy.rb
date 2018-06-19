a9aef044f2b086ea4dcb97aaf114e4713208a2a4
tsai810417 <tsai_cheng@yahoo.com>
2018-06-18 20:54:57 -0700

    solve Maximum Depth of Binary Tree

--- lib/easy.rb
+++ lib/easy.rb
@@ -2301,3 +2301,10 @@ end
 
 # @param {TreeNode} root
 # @return {Integer}
+def max_depth(root)
+    return 0 if root.nil?
+    return 1 if root.left.nil? && root.right.nil?
+    left = max_depth(root.left)
+    right = max_depth(root.right)
+    return [left, right].max + 1
+end
