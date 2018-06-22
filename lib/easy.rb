67d9c39e43cdb2d3e112ae6c7dcf90baeba9943d
tsai810417 <tsai_cheng@yahoo.com>
2018-06-22 00:04:24 -0700

    download Jewels and Stones

--- lib/easy.rb
+++ lib/easy.rb
@@ -2308,3 +2308,32 @@ def max_depth(root)
     right = max_depth(root.right)
     return [left, right].max + 1
 end
+
+# 771. Jewels and Stones
+# You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+#
+# The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+#
+# Example 1:
+#
+# Input: J = "aA", S = "aAAbbbb"
+# Output: 3
+# Example 2:
+#
+# Input: J = "z", S = "ZZ"
+# Output: 0
+# Note:
+#
+# S and J will consist of letters and have length at most 50.
+# The characters in J are distinct.
+# @param {String} j
+# @param {String} s
+# @return {Integer}
+def num_jewels_in_stones(j, s)
+    count = 0
+    s.chars.each do | x |
+        count += 1 if j.include?(x)
+    end
+
+    count
+end
