0355c22b932454617e59768e28c77fb27b7cf299
tsai810417 <tsai_cheng@yahoo.com>
2018-07-07 17:43:35 -0700

    use JS to solve isPowerOfTwo

--- cracking_practice.js
+++ cracking_practice.js
@@ -305,3 +305,13 @@ const findTheDifference = function(str1, str2){
 
   return diff;
 }
+
+const isPowerOfTwo = function(n){
+  if (n === 1){
+    return true;
+  } else if(n === 0 || n % 2 != 0){
+    return false;
+  } else {
+    return isPowerOfTwo(n / 2);
+  }
+}
