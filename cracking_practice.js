8cbe9f4718d753ce566abf0d82c2543dbe35623d
tsai810417 <tsai_cheng@yahoo.com>
2018-07-09 15:51:19 -0700

    use JS to solve mergeTrees

--- cracking_practice.js
+++ cracking_practice.js
@@ -352,3 +352,15 @@ const isIsomorphic = function(s, t){
 
   return translate(s) === translate(t);
 }
+
+const mergeTrees = function(t1, t2){
+  if(t1 === null && t2 === null) return null;
+  if(t1 === null) return t2;
+  if(t2 === null) return t1;
+
+  t1.val += t2.val;
+  t1.left = mergeTrees(t1.left, t2.left);
+  t1.right = mergeTrees(t1.right, t2.right);
+
+  return t1;
+}
